6b7f583b89635c53317ec4886c6d3b7315b752bf
FLASHmlg <atamamchukuriy@gmail.com>
2022-06-24 03:17:41 +0300

    make it better

---
+++ src/components/Header.js
@@ -0,0 +1,187 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../Redux/Actions/userActions";
+
+const Header = () => {
+  const [keyword, setKeyword] = useState();
+  const dispatch = useDispatch();
+  let history = useHistory();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`);
+    } else {
+      history.push("/");
+    }
+  };
+  return (
+    <div className="backgroundColor:#000000">
+      {/* Header */}
+      <div className="header">
+        <div className="container">
+          {/* MOBILE HEADER */}
+          <div className="mobile-header">
+            <div className="container ">
+              <div className="row ">
+                <div className="col-6 d-flex align-items-center">
+                  <Link className="navbar-brand" to="/">
+                    {/* <img alt="logo" src="/images/logo.png" /> */}
+                    Artas
+                  </Link>
+                </div>
+                <div className="col-6 d-flex align-items-center justify-content-end Login-Register">
+                  {userInfo ? (
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="name-button dropdown-toggle"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        <i class="fas fa-user"></i>
+                      </button>
+                      <div className="dropdown-menu">
+                        <Link className="dropdown-item" to="/profile">
+                          Профіль
+                        </Link>
+                        <Link
+                          className="dropdown-item"
+                          to="/http://localhost:8000"
+                        >
+                          Admin
+                        </Link>
+                        <Link
+                          className="dropdown-item"
+                          to="#"
+                          onClick={logoutHandler}
+                        >
+                          Вихід
+                        </Link>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="name-button dropdown-toggle"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        <i class="fas fa-user"></i>
+                      </button>
+                      <div className="dropdown-menu">
+                        <Link className="dropdown-item" to="/login">
+                          ВХІД
+                        </Link>
+
+                        <Link className="dropdown-item" to="/register">
+                          Реєстрація
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+
+                  <Link to="/cart" className="cart-mobile-icon">
+                    <i className="fas fa-shopping-bag"></i>
+                    <span className="badge">{cartItems.length}</span>
+                  </Link>
+                </div>
+                <div className="col-12 d-flex align-items-center">
+                  <form onSubmit={submitHandler} className="input-group">
+                    <input
+                      type="search"
+                      className="form-control rounded search"
+                      placeholder="Search"
+                      onChange={(e) => setKeyword(e.target.value)}
+                    />
+                    <button type="submit" className="search-button">
+                      пошук
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* PC HEADER */}
+          <div className="pc-header">
+            <div className="row">
+              <div className="col-md-3 col-4 d-flex align-items-center">
+                <Link className="navbar-brand" to="/">
+                  {/* <img alt="logo" src="/images/logo.png" /> */}
+                  Artas
+                </Link>
+              </div>
+              <div className="col-md-6 col-8 d-flex align-items-center">
+                <form onSubmit={submitHandler} className="input-group">
+                  <input
+                    type="search"
+                    className="form-control rounded search"
+                    placeholder="Search"
+                    onChange={(e) => setKeyword(e.target.value)}
+                  />
+                  <button type="submit" className="search-button">
+                    знайти
+                  </button>
+                </form>
+              </div>
+              <div className="col-md-3 d-flex align-items-center justify-content-end Login-Register">
+                {userInfo ? (
+                  <div className="btn-group">
+                    <button
+                      type="button"
+                      className="name-button dropdown-toggle"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      {userInfo.name}
+                    </button>
+                    <div className="dropdown-menu">
+                      <Link className="dropdown-item" to="/profile">
+                        Профіль
+                      </Link>
+
+                      <Link
+                        className="dropdown-item"
+                        to="#"
+                        onClick={logoutHandler}
+                      >
+                        Logout
+                      </Link>
+                    </div>
+                  </div>
+                ) : (
+                  <>
+                    <Link to="/register">Реєстрація</Link>
+                    <Link to="/login">ВХІД</Link>
+                  </>
+                )}
+
+                <Link to="/cart">
+                  <i className="fas fa-shopping-bag"></i>
+                  <span className="badge">{cartItems.length}</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Позвоніть нам</h5>
+            <p>063 250 43 56</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Завітайте до нас</h5>
+            <p>вул. 15 Квітня 6, Тернопіль</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>Fax</h5>
+            <p>063 250 4356</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
